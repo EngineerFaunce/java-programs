c5e649ef34abfd01dd5bb027b0399bd597037e23
William Faunce <EngineerFaunce@users.noreply.github.com>
2020-07-26 19:18:46 -0400

    Add BooklistGenerator class

--- COS 125/Assignment 10/Book.java
+++ COS 125/Assignment 10/Book.java
@@ -1,30 +1,28 @@
 // William Faunce
 // Base class for creating book objects
 
-package bookProgram;
-
 public abstract class Book {
-	protected String title;						// title of book
-	protected String author;					// author of book
-	protected int isbn;							// ISBN of book
-	protected double price;						// wholesale price of book
-	
+	protected String title; // title of book
+	protected String author; // author of book
+	protected String isbn; // ISBN of book
+	protected double price; // wholesale price of book
+
 	// default constructor for a book
 	public Book() {
 		title = "";
 		author = "";
-		isbn = 0;
+		isbn = "";
 		price = 0.00;
 	}
-	
+
 	// parameterized constructor for a book
-	public Book(String bookTitle, String bookAuthor, int bookIsbn, double bookPrice) {
+	public Book(String bookTitle, String bookAuthor, String bookIsbn, double bookPrice) {
 		title = bookTitle;
 		author = bookAuthor;
 		isbn = bookIsbn;
 		price = bookPrice;
 	}
-	
+
 	// POST: returns title
 	public String getTitle() {
 		return title;
@@ -34,36 +32,36 @@ public abstract class Book {
 	public String getAuthor() {
 		return author;
 	}
-	
+
 	// POST: returns isbn number
 	public int getIsbn() {
 		return isbn;
 	}
-	
+
 	// POST: returns price
 	public double getPrice() {
 		return price;
 	}
-	
+
 	// POST: sets a new title
 	public void setTitle(String newTitle) {
 		title = newTitle;
 	}
-	
+
 	// POST: sets a new author
 	public void setAuthor(String newAuthor) {
 		author = newAuthor;
 	}
-	
+
 	// POST: sets a new isbn
-	public void setIsbn(int newIsbn) {
+	public void setIsbn(String newIsbn) {
 		isbn = newIsbn;
 	}
-	
+
 	// POST: sets a new price
 	public void setPrice(double newPrice) {
 		price = newPrice;
 	}
-	
+
 	public abstract double retailPrice();
 }
--- COS 125/Assignment 10/Textbook.java
+++ COS 125/Assignment 10/Textbook.java
@@ -1,47 +1,46 @@
 // William Faunce
 // Class Textbook that is derived from Book
 
-package bookProgram;
-
 public class Textbook extends Book {
 	private String course;
 	private double retailPrice;
 	private final double MARKUP = 0.1;
-	
+
 	// constructor for empty textbook
 	public Textbook() {
 		super();
 		course = "";
 		retailPrice = 0.00;
 	}
-	
+
 	// constructor for parameterized textbook
-	public Textbook(String bookTitle, String bookAuthor, int bookIsbn, double bookPrice, String courseID, double retailPr) {
+	public Textbook(String bookTitle, String bookAuthor, String bookIsbn, double bookPrice, String courseID,
+			double retailPr) {
 		super(bookTitle, bookAuthor, bookIsbn, bookPrice);
 		course = courseID;
 		retailPrice = retailPr;
 	}
-	
+
 	// POST: returns course ID associated with the textbook
 	public String getCourseID() {
 		return course;
 	}
-	
+
 	// POST: returns retail price
 	public double getRetailPrice() {
 		return retailPrice;
 	}
-	
+
 	// POST: assigns a new course ID
 	public void setCourseID(String newCourse) {
 		course = newCourse;
 	}
-	
+
 	// POST: assigns a new retail price
 	public void setRetailPrice(double newRP) {
 		retailPrice = newRP;
 	}
-	
+
 	// POST: returns price after adding markup to base price
 	public double retailPrice() {
 		return price + (price * MARKUP);
