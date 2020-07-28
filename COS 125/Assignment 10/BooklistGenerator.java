4ee57888b84117021c5d95d58eba28dd78c4476a
William Faunce <EngineerFaunce@users.noreply.github.com>
2020-07-28 11:08:57 -0400

    Finish BooklistGenerator class

--- COS 125/Assignment 10/BooklistGenerator.java
+++ COS 125/Assignment 10/BooklistGenerator.java
@@ -1,32 +1,54 @@
-import java.io.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.NoSuchElementException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BooklistGenerator {
+    public Book[] booklist; // array that contains Book objects
 
-    public static void main(String[] args) throws IOException {
-        Scanner filescan = new Scanner(new FileReader("books.txt")); // scanner for scanning files
-        NumberFormat price = new DecimalFormat("$0.00"); // format for adding a $ sign for pricing
+    public BooklistGenerator() {
+        booklist = new Book[50];
+    }
 
-        Book[] booklist = new Book[50];
+    /*
+     * Reads in data from books.txt. Assumes that data will be read in the following
+     * order: 1. Title 2. Author 3. ISBN 4. Wholesale price 5. Course ID (e.g.
+     * COS125) or Major (COS)
+     */
+    public void LoadData() throws IOException {
+        Scanner filescan = new Scanner(new FileReader("books.txt")); // scanner for reading books.txt
 
-        while (filescan.hasNext()) {
-            /*
-             * We have an array of type Book. Books have their own attributes.
-             * 
-             * We will have to read the data from books.txt
-             * 
-             * The first line for a book indicates if it is a Textbook or Tradebook. Every
-             * successive line is data for that book.
-             * 
-             * So we just need to check what type of book is being read and then use the
-             * constructor for the relevant book. After constructing the book we will add it
-             * to the booklist array.
-             */
-            if (filescan.next().equals("Textbook")) {
+        String check;
+        int counter = 0;
 
+        while (filescan.hasNext() && counter < 50) {
+            // Checking whether the book is a Textbook or a Tradebook
+            check = filescan.nextLine();
+
+            if (check.equals("Textbook")) {
+                Textbook book = new Textbook();
+
+                book.setTitle(filescan.nextLine());
+                book.setAuthor(filescan.nextLine());
+                book.setIsbn(filescan.nextLine());
+                book.setPrice(Double.parseDouble(filescan.nextLine()));
+                book.setCourseID(filescan.nextLine());
+
+                booklist[counter] = book;
+                counter++;
+            } else if (check.equals("Tradebook")) {
+                Tradebook trBook = new Tradebook();
+
+                trBook.setTitle(filescan.nextLine());
+                trBook.setAuthor(filescan.nextLine());
+                trBook.setIsbn(filescan.nextLine());
+                trBook.setPrice(Double.parseDouble(filescan.nextLine()));
+                trBook.setMajor(filescan.nextLine());
+
+                booklist[counter] = trBook;
+                counter++;
+            } else {
+                System.out.println("Error reading input from file. Expected 'Textbook' or 'Tradebook', got " + check);
+                System.exit(1);
             }
         }
 
